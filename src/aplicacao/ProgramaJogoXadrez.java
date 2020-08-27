3a3fdc3681387058909bd1665d657a034c0f57ac
CLAUDISNEI <claudisneibello@gmail.com>
2020-08-27 13:56:27 -0300

    Primeira classe: Posicao

---
+++ src/aplicacao/ProgramaJogoXadrez.java
@@ -0,0 +1,15 @@
+package aplicacao;
+
+import tabuleiroJogo.Posicao;
+
+public class ProgramaJogoXadrez {
+
+	public static void main(String[] args) {
+		
+		Posicao p = new Posicao(3, 5);
+		
+		System.out.println(p);
+		
+	}
+
+}
